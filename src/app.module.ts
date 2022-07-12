d54396368b9f02d71e0c14a50a9cf67eeea35158
Titux Metal (DEV) <github@lgdweb.fr>
2022-07-13 00:06:40 +0200

    Adds better configuration management using NestJS Config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 
 import { AuthModule } from '~/auth'
 import { PrismaModule } from '~/prisma'
 
 @Module({
-  imports: [AuthModule, PrismaModule]
+  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true, cache: true }), PrismaModule]
 })
 export class AppModule {}
