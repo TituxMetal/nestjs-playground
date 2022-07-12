44d250184d073c02a34572ca81a4617b0fd25187
Titux Metal (DEV) <github@lgdweb.fr>
2022-07-12 08:48:42 +0200

    Implementing the signup service using prisma, dto, validation pipe and hashing password with argon

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, HttpCode, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, Post } from '@nestjs/common'
 
 import { AuthService } from '~/auth/auth.service'
+
+import { AuthDto } from './dto'
 
 @Controller('auth')
 export class AuthController {
@@ -8,8 +10,8 @@ export class AuthController {
 
   @Post('signup')
   @HttpCode(200)
-  signup() {
-    return this.authService.signup()
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto)
   }
 
   @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,37 @@
-import { Injectable } from '@nestjs/common'
+import { ForbiddenException, Injectable } from '@nestjs/common'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
+import * as argon from 'argon2'
 
 import { PrismaService } from '~/prisma/prisma.service'
+
+import { AuthDto } from './dto'
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async signup() {
-    return { message: 'I am signup' }
+  async signup(dto: AuthDto) {
+    const { email, password } = dto
+    const hash = await argon.hash(password)
+
+    try {
+      const newUser = await this.prisma.user.create({
+        data: { email: email, hash },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true
+        }
+      })
+
+      return { data: newUser }
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError && error.code === 'P2002') {
+        throw new ForbiddenException('Credentials already taken')
+      }
+
+      throw error
+    }
   }
 
   async signin() {
