8d5b12be647a28928197e0acd8df64b8b9e56920
Titux Metal (DEV) <github@lgdweb.fr>
2022-07-13 10:41:28 +0200

    Adding the user controller that uses jwt strategy; returns the current logged in user's informations when a bearer token is provided

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { ConfigModule } from '@nestjs/config'
 
 import { AuthModule } from '~/auth'
 import { PrismaModule } from '~/prisma'
+import { UserController } from '~/user'
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true, cache: true }), PrismaModule]
+  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true, cache: true }), PrismaModule],
+  controllers: [UserController]
 })
 export class AppModule {}
