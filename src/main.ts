67774ab24dd8208334cb45b89ec6d5a8e72e9335
Titux Metal (DEV) <github@lgdweb.fr>
2022-07-15 08:53:52 +0200

    Update prisma service; adds a way to close the app properly on prisma exit; adds a way to clean the database

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from '~/app.module'
+import { PrismaService } from '~/prisma'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  const configService = app.get(ConfigService)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+  const prismaService = app.get(PrismaService)
+  await prismaService.enableShutdownHooks(app)
+
+  const configService = app.get(ConfigService)
   await app.listen(configService.get('PORT'))
 }
 bootstrap()
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common'
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { PrismaClient } from '@prisma/client'
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor(config: ConfigService) {
     super({
       datasources: {
@@ -12,5 +12,19 @@ export class PrismaService extends PrismaClient {
         }
       }
     })
+  }
+
+  async onModuleInit() {
+    await this.$connect()
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close()
+    })
+  }
+
+  cleanDb() {
+    return this.$transaction([this.user.deleteMany()])
   }
 }
