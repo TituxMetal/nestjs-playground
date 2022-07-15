a9a097cde3fd469da61930b4183c4ac59d74e27d
Titux Metal (DEV) <github@lgdweb.fr>
2022-07-15 13:46:57 +0200

    Adding end to end test for updating a user profile

--- test/user.ts
+++ test/user.ts
@@ -1,5 +1,8 @@
 import { HttpStatus } from '@nestjs/common'
 import * as pactum from 'pactum'
+import { like } from 'pactum-matchers'
+
+import { UpdateUserProfileDto } from '~/user'
 
 export const UserTest = () =>
   describe('User module', () => {
@@ -20,5 +23,32 @@ export const UserTest = () =>
           .get('/users/me')
           .withHeaders({ Authorization: 'Bearer $S{userAccessToken}' })
           .expectStatus(HttpStatus.OK))
+    })
+
+    describe('Update user profile', () => {
+      const dto: UpdateUserProfileDto = {
+        firstName: 'Titux',
+        lastName: 'Metal',
+        email: 'tituxmetal@lgdweb.fr'
+      }
+
+      it('should throw if no token provided', async () =>
+        pactum.spec().patch('/users').expectStatus(HttpStatus.UNAUTHORIZED))
+
+      it('should throw if invalid token provided', async () =>
+        pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({ Authorization: 'Bearer azerty123' })
+          .expectStatus(HttpStatus.UNAUTHORIZED))
+
+      it('should update the user profile', async () =>
+        pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({ Authorization: 'Bearer $S{userAccessToken}' })
+          .withBody(dto)
+          .expectStatus(HttpStatus.OK)
+          .expectJsonMatch({ id: like('aze'), ...dto }))
     })
   })
